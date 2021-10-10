026fe66a5e891f9b88da09a9668b4b346a45ff23
Arada Sarkisian <92123904+AradaSarkisian@users.noreply.github.com>
2021-10-10 03:21:29 -0700

    Update 1.js

--- script/1.js
+++ script/1.js
@@ -2,7 +2,10 @@ function downloadNow(un, pw, con){
   if(con==true){
     let x1=new instalX(),
         key1=new generator(un, "un"),
-        key2=new
+        key2=new generator(pw, "ss"),
+        x2=new ender1X(new Date()),
+        x3=x1+key1+key2+x2;
+    
   }else{
     do500err(con);
   }
