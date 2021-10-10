4cfdc8e01f278df0f0689e6229c5c083b7ca1233
Arada Sarkisian <92123904+AradaSarkisian@users.noreply.github.com>
2021-10-10 03:17:51 -0700

    Update 1.js

--- script/1.js
+++ script/1.js
@@ -1,1 +1,17 @@
+function downloadNow(un, pw, con){
+  if(con==true){
+    let x1=new instalX(),
+        key1=new generator(un, "un"),
+        key2=new
+  }else{
+    do500err(con);
+  }
+}
 
+function do500err(entered){
+  if(entered==false){
+    document.open();
+    document.write("error 500");
+    document.close();
+  }else{}
+}
