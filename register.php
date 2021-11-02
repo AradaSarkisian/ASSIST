1e0cec2078522f05ae3f5706a275a3c5b910663a
Arada Sarkisian <92123904+AradaSarkisian@users.noreply.github.com>
2021-11-02 14:19:19 -0700

    Update register.php

--- register.php
+++ register.php
@@ -3,14 +3,14 @@ $username=$_POST["username"];
 $email=$_POST["email"];
 if (!empty($_POST["button"]) && !empty($username) && !empty($email)) {
     $servername = "localhost";
-    $username = "username";
-    $password = "password";
+    $Susername = "username";
+    $Spassword = "password";
     $dbname = "myDB";
     $condition1=false;
     $TextX1="Registeration failed";
     $ColorX1="red";
     $ScriptX1="";
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $Susername, $Spassword, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -43,16 +43,16 @@ function generateRandomString($length = 15) {
 function callIfAllIsOK() {
     $TextX1="You was registered now";
     $ColorX1="green";
-    $ScriptX1="scipts/1.js";
-    $CallX1="downloadNow(".$random_key.",".$condition1.")";
+    $ScriptX1="scipts/2.js";
+    $CallX1="downloadNow(".$username.",".$random_key.",".$condition1.")";
 }
 ?>
 <!DOCTYPE html>
 <html>
     <title>Server</title>
-    <link href="style/1.css" type="text/stylesheet"/>
+    <link href="style/1.css" type="text/stylesheet" rel="stylesheet"/>
     <script src="<?php echo $ScriptX1; ?>"></script>
-    <script src="script/2.js"></script>
+    <script src="script/1.js"></script>
 </html>
 <body>
     <!--Install source here-->
